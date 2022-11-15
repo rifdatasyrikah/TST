91f0bd4da82749db1e71cc8590af164c627a3382
rifdatasyrikah <rifda.tasyrikah@gmail.com>
2022-11-15 08:40:47 +0700

    coba buat dockerfile

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.9.7
+WORKDIR /Users/rifda/TST
+ADD . /Users/rifda/TST/
+CMD ["python", "main.py"]
---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import json
+
+app = FastAPI()
+
+class mahasiswa(BaseModel):
+    nim: int
+    nama: str
+
+@app.post("/")
+# menerima data NIM dan nama dar client dalam format JSON
+async def add_mahasiswa(M: mahasiswa):
+    #inisialisasi list data mahasiswa
+    list_mahasiswa = []
+
+    #simpan data dari file JSON ke list
+    with open('mahasiswa.json', 'r') as f:
+        list_mahasiswa = json.load(f)['mahasiswa']
+
+    #cek isi data awal
+    print(list_mahasiswa)
+
+    #disimpan ke dalam sebuah variabel (dictionary)
+    new_mahasiswa = {
+        "nim":M.nim,
+        "nama":M.nama
+        }
+
+    # #menambahkan data baru ke data mahasiswa yang sudah ada
+    list_mahasiswa.append(new_mahasiswa)
+
+    #cek list data baru
+    print(list_mahasiswa)
+
+    #menyimpan seluruh data ke file JSON
+    luaran = {'mahasiswa':list_mahasiswa}
+    with open('mahasiswa.json', 'w') as f:
+        json.dump(luaran, f)
+
+    return "Data mahasiswa baru: " +str(new_mahasiswa)+ " telah diterima"
+
+#asumsi input tidak perlu dicek apakah data nim yang diinput sudah ada pada data yang ada sebelumnya
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
